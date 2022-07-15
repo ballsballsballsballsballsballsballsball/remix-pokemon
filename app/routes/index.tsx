1040340134609e8182985157e5b3444fcedd9b63
Soren <sorenzio69@gmail.com>
2022-07-15 00:28:31 -0500

    wow this is so much better than the trpc version

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,46 @@
-import { Title } from "@mantine/core";
+import { Card, Grid, Image, Text, Title } from "@mantine/core";
+import { useFetcher } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { jsonTyped, useLoaderDataTyped } from "~/utils";
+
+type PokemonResponse = {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
+
+type PokemonByNameResponse = {
+  sprites: {
+    front_default: string;
+    versions: {
+      "generation-v": {
+        "black-white": {
+          animated: {
+            front_default: string;
+          };
+        };
+      };
+    };
+  };
+};
+
+export async function loader() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
+
+  const data: PokemonResponse = await response.json();
+
+  return jsonTyped({
+    pokemon: data.results,
+  });
+}
 
 export default function Index() {
+  const { pokemon } = useLoaderDataTyped<typeof loader>();
+
   return (
     <div
       style={{
@@ -13,6 +53,63 @@ export default function Index() {
       }}
     >
       <Title>Poke man</Title>
+
+      <Grid
+        grow
+        sx={{
+          width: "80%",
+        }}
+      >
+        {pokemon.map((poke) => {
+          return (
+            <Grid.Col key={poke.url} span={12} lg={2}>
+              <Card
+                sx={{
+                  textAlign: "center",
+                  textTransform: "capitalize",
+                }}
+              >
+                <Pokemon name={poke.name} />
+              </Card>
+            </Grid.Col>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
+
+const Pokemon = ({ name }: { name: string }) => {
+  const [pokemon, setPokemon] = useState<PokemonByNameResponse | null>(null);
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const json = await data.json();
+      setPokemon(json);
+    };
+
+    fetchPokemon();
+  }, [name]);
+
+  return (
+    <>
+      {pokemon &&
+        (pokemon.sprites.versions["generation-v"]["black-white"].animated
+          .front_default ??
+          pokemon.sprites.front_default) && (
+          <Image
+            src={
+              pokemon.sprites.versions["generation-v"]["black-white"].animated
+                .front_default ?? pokemon.sprites.front_default
+            }
+            sx={{
+              imageRendering: "pixelated",
+            }}
+            alt={name}
+            height={200}
+          />
+        )}
+      <Text>{name}</Text>
+    </>
+  );
+};
--- app/utils.ts
+++ app/utils.ts
@@ -1,14 +1,17 @@
+import { json } from "@remix-run/node";
+import { useActionData, useLoaderData } from "@remix-run/react";
+
 /// https://gist.github.com/esamattis/d1172371b0efc1327b8617de6e044f64
 export function useLoaderDataTyped<T extends (arg: any) => any>(): Awaited<
   ReturnType<T>
 > {
-  return useLoaderData();
+  return useLoaderData() as Awaited<ReturnType<T>>;
 }
 
 export function useActionDataTyped<T extends (arg: any) => any>():
   | Awaited<ReturnType<T>>
   | undefined {
-  return useActionData();
+  return useActionData() as Awaited<ReturnType<T>> | undefined;
 }
 
 export function jsonTyped<T>(data: T, init?: Parameters<typeof json>[1]): T {
